b88c7b39e0bbc70410bebf4c55e96c3270460c94
Elielson68 <elielsonbr.com@gmail.com>
2020-09-20 14:54:08 -0300

    feat: CPU salvando jogadas com vitórias feat: CPU selecionando alguma das jogadas aleatoriamente a cada novo jogo

--- PlayJogo.py
+++ PlayJogo.py
@@ -6,6 +6,7 @@ tabu = Tabuleiro()
 jogador1 = Jogador("Player 1", "X")
 jogador2 = Jogador("Player 2", "O")
 CPU = CPU()
+
 while True:
     MenuOpcoes()
     usuario = int(input("Você: "))
@@ -36,20 +37,27 @@ while True:
     elif usuario==2:
         tabu.setJogadores(jogador1)
         tabu.setJogadores(CPU)
+        CPU.CarregarJogadas()
+        CPU.SelecionarJogada()
         while True:
             print(tabu.MostrarTabuleiro())
             if tabu.isGanhador():
                 tabu.setVez(tabu.RevezarVez())
                 print("O ganhador é ", tabu.getNomeJogadorDaVez())
+                CPU.ResetarCPU()
+                if tabu.getNomeJogadorDaVez() == CPU.getNome():
+                    if not CPU.isJogadaEmArquivo():
+                        CPU.SalvarJogada()
                 usuario = JogarNovamente(tabu, jogador1, CPU)
-                if usuario==1:
+                if usuario == 1:
                     continue
                 if usuario == 2:
                     break
             if not tabu.isCasasDisponiveis():
                 print("Não houve ganhadores! Empate!")
                 usuario = JogarNovamente(tabu, jogador1, CPU)
-                if usuario==1:
+                CPU.ResetarCPU()
+                if usuario == 1:
                     continue
                 if usuario == 2:
                     break
@@ -58,8 +66,11 @@ while True:
             if tabu.getNomeJogadorDaVez()==jogador1.getNome():
                 jogada = input("Digite sua jogada: ")
             else:
-                movimento_CPU = CPU.getMovimentoCPU()
-                jogada = tabu.getListKeysDecodificador()[movimento_CPU]
+                jogada = CPU.ProximaJogada()
+                print(jogada)
+                if not tabu.isJogadaValida(jogada):
+                    movimento_CPU = CPU.getMovimentoCPU()
+                    jogada = tabu.getListKeysDecodificador()[movimento_CPU]
             tabu.setJogada(jogada)
         tabu.RemoverJogadores()
     else:
--- Tabuleiro/cpu.py
+++ Tabuleiro/cpu.py
@@ -1,10 +1,52 @@
 from .jogador import Jogador
 from random import randint
+import json
 
 class CPU(Jogador):
     def __init__(self, nome=None, simbolo=None):
         super().__init__(nome or "CPU", simbolo or "O")
+        self.arquivo_com_jogadas = None
+        self.list_de_jogadas_futuras = []
+        self.jogadas_futuras = None
+        self.proxima_jogada = -1
 
     def getMovimentoCPU(self):
         movimento = randint(0, 8)
         return movimento
+
+    def SalvarJogada(self):
+        self.arquivo_com_jogadas = open("Tabuleiro/Jogadas_CPU/jogadas.txt", "a")
+        jogadas_em_string = str(self.getMovimento())+'\n'
+        self.arquivo_com_jogadas.write(jogadas_em_string)
+        self.arquivo_com_jogadas.close()
+
+    def CarregarJogadas(self):
+        self.arquivo_com_jogadas = open("Tabuleiro/Jogadas_CPU/jogadas.txt", "r")
+        self.list_de_jogadas_futuras = []
+        for linha in self.arquivo_com_jogadas:
+            self.list_de_jogadas_futuras.append(eval(linha))
+        self.arquivo_com_jogadas.close()
+
+    def SelecionarJogada(self):
+        tamanho_lista_jogadas = len(self.list_de_jogadas_futuras) - 1
+        selecionar_jogada = randint(0, tamanho_lista_jogadas)
+        self.jogadas_futuras = self.list_de_jogadas_futuras[selecionar_jogada]
+
+    def ProximaJogada(self):
+        if not self.isLimiteJogadasFuturas(self.proxima_jogada):
+            self.proxima_jogada += 1
+        return self.jogadas_futuras[self.proxima_jogada]
+
+    def isLimiteJogadasFuturas(self, valor):
+        if valor > len(self.jogadas_futuras)-1:
+            return True
+        else:
+            return False
+    def isJogadaEmArquivo(self):
+        return self.movimentos in self.list_de_jogadas_futuras
+
+    def ResetarCPU(self):
+        self.arquivo_com_jogadas = None
+        self.list_de_jogadas_futuras = []
+        self.jogadas_futuras = None
+        self.proxima_jogada = -1
